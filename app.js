806281e9fed5456b1828b882a1339af18befd7dc
Henry Wang <henry.wang@live.com>
2019-04-02 05:52:46 +0000

    styling

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var methodOverride = require('method-override');
+var expressSanitizer = require('express-sanitizer');
 
 //App Config
 mongoose.connect('mongodb://localhost/restful_blog_app', { useNewUrlParser: true });
@@ -11,6 +12,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
+app.use(expressSanitizer());
 
 //Mongoose Model Config
 var blogSchema = new mongoose.Schema({
@@ -20,14 +22,6 @@ var blogSchema = new mongoose.Schema({
    created: {type: Date, default: Date.now}
 });
 var Blog = mongoose.model('Blog', blogSchema);
-
-/*
-Blog.create({
-    title: "Test Blog",
-    image: "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    body: "A blog post"
-})
-*/
 
 
 //RESTful Routes
@@ -55,6 +49,9 @@ app.get('/blogs/new', function(req, res){
 //Create Route
 app.post('/blogs', function(req, res){
    //create blog
+   
+   req.body.blog.body = req.sanitize(req.body.blog.body);
+   
    Blog.create(req.body.blog, function(err, newBlog){
        if(err){
            res.render('new');
@@ -89,6 +86,9 @@ app.get('/blogs/:id/edit', function(req, res){
 
 //Update Route
 app.put('/blogs/:id', function(req, res){
+   
+   req.body.blog.body = req.sanitize(req.body.blog.body);
+   
    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
        if(err){
            res.redirect('/blogs');
